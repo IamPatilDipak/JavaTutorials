ecb8d3453a5912686186634db62f65eac06145b2
iampatildipak <iampatildipak@github.com>
2020-01-16 07:32:27 +0530

    For vs While loop

--- ForLoopWhileLoop/src/com/iampatildipak/Main.java
+++ ForLoopWhileLoop/src/com/iampatildipak/Main.java
@@ -3,6 +3,14 @@ package com.iampatildipak;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("For : " + i);
+        }
+
+        int i = 1;
+        while (i <= 10) {
+            System.out.println("While : " + i);
+            i++;
+        }
     }
 }
