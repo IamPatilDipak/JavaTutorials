2f200e18cad77b7bbf0725fa14f6687a5233a298
iampatildipak <iampatildipak@github.com>
2020-01-17 07:09:42 +0530

    NumberInWord added

---
+++ NumberInWord/src/com/iampatildipak/Main.java
@@ -0,0 +1,14 @@
+package com.iampatildipak;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // EXAMPLE:
+        // printNumberInWord(6)  -> should print "SIX"
+        // printNumberInWord(10)  -> should print "OTHER"
+        // printNumberInWord(-1)  -> should print "OTHER"
+        NumberInWord.printNumberInWord(6);
+        NumberInWord.printNumberInWord(10);
+        NumberInWord.printNumberInWord(-1);
+    }
+}
