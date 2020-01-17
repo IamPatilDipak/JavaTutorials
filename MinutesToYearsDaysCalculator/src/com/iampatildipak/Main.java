b53f133c00931e3747f15f1758302eed91d57a3a
iampatildipak <iampatildipak@github.com>
2020-01-17 06:37:10 +0530

    MinutesToYearsDaysCalculator added

---
+++ MinutesToYearsDaysCalculator/src/com/iampatildipak/Main.java
@@ -0,0 +1,18 @@
+package com.iampatildipak;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // EXAMPLES OF INPUT/OUTPUT:
+        //
+        //* printYearsAndDays(525600);  → should print "525600 min = 1 y and 0 d"
+        //
+        //* printYearsAndDays(1051200); → should print "1051200 min = 2 y and 0 d"
+        //
+        //* printYearsAndDays(561600);  → should print "561600 min = 1 y and 25 d"
+
+        MinutesToYearsDaysCalculator.printYearsAndDays(525600);
+        MinutesToYearsDaysCalculator.printYearsAndDays(1051200);
+        MinutesToYearsDaysCalculator.printYearsAndDays(561600);
+    }
+}
