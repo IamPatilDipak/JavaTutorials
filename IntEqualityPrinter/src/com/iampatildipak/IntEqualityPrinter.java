d52fed28914e5f14eabd2438896776dccf71a542
iampatildipak <iampatildipak@github.com>
2020-01-17 06:49:44 +0530

    IntEqualityPrinter added

---
+++ IntEqualityPrinter/src/com/iampatildipak/IntEqualityPrinter.java
@@ -0,0 +1,18 @@
+package com.iampatildipak;
+
+public class IntEqualityPrinter {
+    public static void printEqual(int first, int second, int third) {
+        if (first < 0 || second < 0 || third < 0) {
+            System.out.println("Invalid Value");
+            return;
+        }
+
+        if (first == second && second == third && third == first) {
+            System.out.println("All numbers are equal");
+        } else if (first != second && second != third && third != first) {
+            System.out.println("All numbers are different");
+        } else {
+            System.out.println("Neither all are equal or different");
+        }
+    }
+}
---
+++ IntEqualityPrinter/src/com/iampatildipak/Main.java
@@ -0,0 +1,21 @@
+package com.iampatildipak;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // EXAMPLES OF INPUT/OUTPUT:
+        //
+        //* printEqual(1, 1, 1); should print text All numbers are equal
+        //
+        //* printEqual(1, 1, 2); should print text Neither all are equal or different
+        //
+        //* printEqual(-1, -1, -1); should print text Invalid Value
+        //
+        //* printEqual(1, 2, 3); should print text All numbers are different
+
+        IntEqualityPrinter.printEqual(1, 1, 1);
+        IntEqualityPrinter.printEqual(1, 1, 2);
+        IntEqualityPrinter.printEqual(-1, -1, -1);
+        IntEqualityPrinter.printEqual(1, 2, 3);
+    }
+}
