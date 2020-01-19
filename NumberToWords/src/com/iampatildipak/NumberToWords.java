982fc380bc702b623f907f6e43afd84f9be01f5b
iampatildipak <iampatildipak@github.com>
2020-01-19 21:04:33 +0530

    NumberToWords added.

---
+++ NumberToWords/src/com/iampatildipak/NumberToWords.java
@@ -0,0 +1,9 @@
+package com.iampatildipak;
+
+public class NumberToWords {
+    public static void numberToWords(int number) {
+        if (number < 0) {
+            System.out.println("Invalid Value");
+        }
+    }
+}
