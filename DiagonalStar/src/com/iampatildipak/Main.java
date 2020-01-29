fd58761dece5af767d77c79bbfd247bb0793cafb
iampatildipak <iampatildipak@github.com>
2020-01-29 07:57:30 +0530

    DiagonalStar added

---
+++ DiagonalStar/src/com/iampatildipak/Main.java
@@ -0,0 +1,36 @@
+package com.iampatildipak;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // EXAMPLE INPUT/OUTPUT:
+        //
+        //EXAMPLE 1
+        //
+        //printSquareStar(5); should print the following:
+        //
+        //*****
+        //** **
+        //* * *
+        //** **
+        //*****
+
+        DiagonalStar.printSquareStar(5);
+
+        //EXAMPLE 2
+        //
+        //printSquareStar(8); should print the following:
+        //
+        //********
+        //**    **
+        //* *  * *
+        //*  **  *
+        //*  **  *
+        //* *  * *
+        //**    **
+        //********
+
+        DiagonalStar.printSquareStar(8);
+
+    }
+}
