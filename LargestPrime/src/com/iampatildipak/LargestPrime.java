062151a5f8ed4dafd6702f2a69d840a288fc86b0
iampatildipak <iampatildipak@github.com>
2020-01-29 07:51:37 +0530

    LargestPrime added

---
+++ LargestPrime/src/com/iampatildipak/LargestPrime.java
@@ -0,0 +1,17 @@
+package com.iampatildipak;
+
+public class LargestPrime {
+    public static int getLargestPrime(int number) {
+
+        if (number >= 2) {
+            for (int i = 2; i < number; i++) {
+                if (number % i == 0) {
+                    number /= i;
+                    i--;
+                }
+            }
+            return number;
+        }
+        return -1;
+    }
+}
---
+++ LargestPrime/src/com/iampatildipak/Main.java
@@ -0,0 +1,24 @@
+package com.iampatildipak;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // EXAMPLE INPUT/OUTPUT:
+        //
+        //* getLargestPrime (21); should return 7 since 7 is the largest prime (3 * 7 = 21)
+        //
+        //* getLargestPrime (217); should return 31 since 31 is the largest prime (7 * 31 = 217)
+        //
+        //* getLargestPrime (0); should return -1 since 0 does not have any prime numbers
+        //
+        //* getLargestPrime (45); should return 5 since 5 is the largest prime (3 * 3 * 5 = 45)
+        //
+        //* getLargestPrime (-1); should return -1 since the parameter is negative
+
+        System.out.println(LargestPrime.getLargestPrime(21));
+        System.out.println(LargestPrime.getLargestPrime(217));
+        System.out.println(LargestPrime.getLargestPrime(0));
+        System.out.println(LargestPrime.getLargestPrime(45));
+        System.out.println(LargestPrime.getLargestPrime(-1));
+    }
+}
